2a49de505f3c3548833a2542b6a4954abd77fa3c
Rajesh Jain <rjain15@gmail.com>
2017-05-30 20:31:25 -0400

    Remove and cleanup firebase dependencies

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {AngularFire} from 'angularfire2';
 import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
@@ -10,15 +9,11 @@ import {FlashMessagesService} from 'angular2-flash-messages';
 export class HomeComponent implements OnInit {
 
   constructor(
-    public af:AngularFire,
     public flashMessage:FlashMessagesService
   ) { }
 
   ngOnInit() {
   }
 
-  login(){
-    this.af.auth.login();
-  }
 
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {AngularFire} from 'angularfire2';
 import {FlashMessagesService} from 'angular2-flash-messages';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 
@@ -11,7 +10,6 @@ import {Router, ActivatedRoute, Params} from '@angular/router';
 export class NavbarComponent implements OnInit {
 
   constructor(
-    public af:AngularFire,
     public flashMessage:FlashMessagesService,
     private router:Router,
     private route:ActivatedRoute
@@ -20,15 +18,5 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
   }
 
-  login(){
-    this.af.auth.login();
-  }
-
-  logout(){
-    this.af.auth.logout();
-    this.router.navigate(['/']);
-    this.flashMessage.show('You are logged out',
-    {cssClass: 'alert-success', timeout: 3000});
-  }
 
 }
