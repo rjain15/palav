0084eb998a78a8baffc320c3a372ddc20eac3175
Rajesh Jain <rjain15@gmail.com>
2017-06-01 20:31:46 -0400

    Added Firebase Config

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCVafufcWFhK7giDmUiVxOHTP6opU0BGgI",
-  authDomain: "fir-crud-e16f1.firebaseapp.com",
-  databaseURL: "https://fir-crud-e16f1.firebaseio.com",
-  projectId: "fir-crud-e16f1",
-  storageBucket: "fir-crud-e16f1.appspot.com",
-  messagingSenderId: "259090326125"
+  apiKey: "AIzaSyCeQXSB9d5ihY4v3ke8A38YrDFG5dwPqdc",
+  authDomain: "palav-e8b2f.firebaseapp.com",
+  databaseURL: "https://palav-e8b2f.firebaseio.com",
+  projectId: "palav-e8b2f",
+  storageBucket: "palav-e8b2f.appspot.com",
+  messagingSenderId: "96014704562"
 };
