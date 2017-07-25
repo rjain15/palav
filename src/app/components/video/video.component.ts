1b96ab5337399df98626c619310c9f2d309ccf32
temp8101 <temp8101@gmail.com>
2017-07-24 20:26:18 -0400

    video url chnage

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoComponent implements OnInit {
 
-  youtubelink:string = 'https://www.youtube.com/embed/wdY2bEh9XbE?list=PL7hPeebx3kz3VVLUWdChx0KG6PWyKddk4';
+  youtubelink:string = 'https://www.youtube.com/embed/wdY2bEh9XbE?listType=playlist&list=PL7hPeebx3kz3VVLUWdChx0KG6PWyKddk4';
   constructor() { }
 
   ngOnInit() {
