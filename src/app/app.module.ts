c007dab90271b95d92c51f1df6014ee19fb31794
rjain15 <rjain15@gmail.com>
2017-08-01 14:47:53 -0400

    Video Component Updates, Do npm install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../node_modules/@types/youtube/index.d.ts" />
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -1,16 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.css']
+
 })
+
 export class VideoComponent implements OnInit {
 
-  youtubelink:string = 'https://www.youtube.com/embed/videoseries?list=PLbTNZNtSmrpoUVan0LVqTP3qu_9_aMr6P';
-  constructor() { }
+  private id: string = 'qDuKsiwS5xw';
+  p: YT.Player;
+  done = false;
+
+  youtubelink: string = 'https://www.youtube.com/embed/videoseries?list=PLbTNZNtSmrpoUVan0LVqTP3qu_9_aMr6P';
+
+  constructor() {
+
+    this.p = new YT.Player('player', {
+        height: 390,
+        width: 640,
+        videoId: 'M7lc1UVf-VE',
+        events: {
+            'onReady': this.onPlayerReady,
+            'onStateChange': this.onStateChange
+        }
+    });
+  }
 
   ngOnInit() {
+
+  }
+
+  onStateChange(event) {
+    console.log('player state', event.data);
+  }
+  // 4. The API will call this function when the video player is ready.
+  onPlayerReady(event) {
+    event.target.playVideo();
+  }
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !this.done) {
+      setTimeout(this.stopVideo, 6000);
+      this.done = true;
+    }
+  }
+
+  stopVideo() {
+    this.p.stopVideo();
   }
 
 }
